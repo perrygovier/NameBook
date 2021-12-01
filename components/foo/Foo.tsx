b5973c12e4c1457e368c463b574ecf9cf1e1eb5c
perry <perry@ionic.io>
2021-11-30 20:49:37 -0600

    Converting to TS, adding SCSS support, & SVG imports

---
+++ components/foo/Foo.tsx
@@ -0,0 +1,10 @@
+
+import styles from './Foo.module.scss';
+
+export default function Foo() {
+  
+
+  return (
+    <h1 className={styles.testing}>Bar</h1>
+  )
+}
--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: { ref: true },
+        },
+      ],
+    });
+
+    return config;
+  }
 }
